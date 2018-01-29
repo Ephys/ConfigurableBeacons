9c0c21b3c5a9da88e492f04902628259565375c2
Guylian Cox <gc@pictawall.com>
2018-01-29 21:12:21 +0100

    build: Upgrade gradle config

--- src/main/java/be/ephys/configurablebeacons/ConfigurableBeacons.java
+++ src/main/java/be/ephys/configurablebeacons/ConfigurableBeacons.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class ConfigurableBeacons
 {
     public static final String MODID = "configurablebeacons";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "@VERSION@";
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
