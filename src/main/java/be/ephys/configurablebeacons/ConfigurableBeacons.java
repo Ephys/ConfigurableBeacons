567e145c6ee2aa3796b2b6f8183a741de17335df
Guylian Cox <gc@pictawall.com>
2018-01-29 21:51:43 +0100

    fix: Mark mod as optional on client/server for real

--- src/main/java/be/ephys/configurablebeacons/ConfigurableBeacons.java
+++ src/main/java/be/ephys/configurablebeacons/ConfigurableBeacons.java
@@ -4,12 +4,14 @@ import be.ephys.cookiecore.config.ConfigSynchronizer;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.network.NetworkCheckHandler;
+import net.minecraftforge.fml.relauncher.Side;
+
+import java.util.Map;
 
 @Mod(
     modid = ConfigurableBeacons.MODID,
     version = ConfigurableBeacons.VERSION,
-    serverSideOnly = true,
-    clientSideOnly = true,
     certificateFingerprint = "@FINGERPRINT@",
     dependencies="required-after:cookiecore@[2.0.0,);"
 )
@@ -17,6 +19,13 @@ public class ConfigurableBeacons
 {
     public static final String MODID = "configurablebeacons";
     public static final String VERSION = "@VERSION@";
+
+    @NetworkCheckHandler
+    public boolean acceptConnection(Map<String, String> modList, Side side) {
+
+        // Mod can be used on both client & server even if the other party doesn't have it installed
+        return true;
+    }
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
